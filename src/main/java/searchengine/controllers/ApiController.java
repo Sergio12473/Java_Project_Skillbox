e7d8488d36f28749737df8600cda30f471f6e8ae
Sergio <shakin.s@yandex.ru>
2026-02-10 20:41:39 +0300

    SearchEngine_Skillbox Доработка проекта.

--- src/main/java/searchengine/controllers/ApiController.java
+++ src/main/java/searchengine/controllers/ApiController.java
@@ -2,7 +2,6 @@ package searchengine.controllers;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import searchengine.dto.index.IndexResponse;
 import searchengine.dto.search.SearchRequest;
--- src/main/java/searchengine/repositories/LemmaRepository.java
+++ src/main/java/searchengine/repositories/LemmaRepository.java
@@ -3,7 +3,6 @@ package searchengine.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import searchengine.model.Lemma;
 import searchengine.model.Site;
 
